a7a415f1b1ff30108643ece09174f3e61824bf0d
ahojo <ahojodev@gmail.com>
2021-08-17 16:36:02 -0400

    Added a rate limiter based on how many requests are happening

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -10,12 +10,11 @@ func (app *application) logError(r *http.Request, err error) {
 	// app.logger.Println(err)
 	app.logger.PrintError(err, map[string]string{
 		"request_method": r.Method,
-		"request_url": r.URL.String(),
+		"request_url":    r.URL.String(),
 	})
 }
 
-
-// errorResponse send JSON formatted error messages to the client with status code. 
+// errorResponse send JSON formatted error messages to the client with status code.
 func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message interface{}) {
 	env := envelope{
 		"error": message,
@@ -23,7 +22,7 @@ func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, st
 
 	// Write the response with writeJSON
 	// If this fails fall back nd send the client an empty response with status 500
-	err := app.writeJSON(w, status, env, nil) 
+	err := app.writeJSON(w, status, env, nil)
 	if err != nil {
 		app.logError(r, err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -36,31 +35,35 @@ func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Reque
 	message := "the server encountered a problem and could not process your request"
 
 	app.errorResponse(w, r, http.StatusInternalServerError, message)
-} 
+}
 
 func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
 	message := "the requested resource could not be found"
-	app.errorResponse(w,r,http.StatusNotFound, message)
+	app.errorResponse(w, r, http.StatusNotFound, message)
 }
 
 // methodNotAllowed sends a 405
 func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
 	message := fmt.Sprintf("The %s method is not supported for this resource", r.Method)
-	app.errorResponse(w,r,http.StatusMethodNotAllowed, message)
+	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
 }
 
 func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
-	app.errorResponse(w,r, http.StatusBadRequest, err.Error())
+	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 }
 
 // Note that the errors parameter here has the type map[string]string which is exactly
 // the same as the errors map contained in our Validator type
-func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string){
+func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
 
-	app.errorResponse(w,r,http.StatusUnprocessableEntity, errors)
+	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
 }
 
 func (app *application) ErrEditConflictResponse(w http.ResponseWriter, r *http.Request) {
 	message := `unable to update the record due to an edit conflict, please try again`
-	app.errorResponse(w,r,http.StatusConflict, message)
+	app.errorResponse(w, r, http.StatusConflict, message)
+}
+func (app *application) rateLimitExceededResponse(w http.ResponseWriter, r *http.Request) {
+	message := "rate limit exceeded"
+	app.errorResponse(w, r, http.StatusTooManyRequests, message)
 }
